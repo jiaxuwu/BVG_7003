96e1024f662125d750ec601d6a245e6f146a676a
Jiaxu Wu <99823441+jiaxuwu@users.noreply.github.com>
2022-10-19 00:35:19 -0400

    Update count_fasta-jxw.py

--- count_fasta-jxw.py
+++ count_fasta-jxw.py
@@ -2,7 +2,7 @@
 
 # Using Bio.SeqIO to count fasta records
 from Bio import SeqIO
-filename = "NC_000913.faa". # Write the your file name here 
+filename = "NC_000913.faa" # Write the your file name here 
 count = 0
 for record in SeqIO.parse (filename, "fasta"):
     count = count +1
