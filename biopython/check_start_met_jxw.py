30df7b05eebe58f4a480af8e001de965284ced4a
Jiaxu Wu <99823441+jiaxuwu@users.noreply.github.com>
2022-11-17 16:53:58 -0500

    Create check_start_met_jxw.py

---
+++ biopython/check_start_met_jxw.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Checking proteins start with methionine
+from Bio import SeqIO
+filename = "NC_000913.faa"  # Write your file name here
+bad = 0
+for record in SeqIO.parse(filename, "fasta"):
+    if not record.seq.startswith("M"):
+        bad = bad + 1
+        print(record.id + " starts " + record.seq(0))
+print("Found " + str(bad) + " records in " + "filename" + " which did not start with M")
