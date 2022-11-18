83043cb0ecc07bde0db99cafb7a68cc6c6b19c97
Jiaxu Wu <99823441+jiaxuwu@users.noreply.github.com>
2022-11-18 00:10:32 -0500

    Delete phylogenetics_3.py

--- phylogentics/phylogenetics_3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# use the function "draw" to create a simple graphic dendrogram
-from Bio import Phylo
-tree = Phylo.read("simple.dnd", "newick")
-tree.rooted = True
-Phylo.draw(tree)
