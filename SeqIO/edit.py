36a3dc4070e440eee8fa96b03d7877fbc701037b
Jiaxu Wu <99823441+jiaxuwu@users.noreply.github.com>
2022-11-21 02:11:49 -0500

    Update edit.py

--- SeqIO/edit.py
+++ SeqIO/edit.py
@@ -2,7 +2,7 @@
 
 # Pulling out particular sequences from a large sequence file
 from Bio import SeqIO
-input_filename = "PGSC_DM_v3.4_pep_representative.fasta"
+input_filename = "PGSC_DM_v3.4_pep_representative.fasta" 
 output_filename = "PGSC_DM_v3.4_pep_rep_no_stars.fasta"
 output_handle = open(output_filename, "w")
 for record in SeqIO.parse(input_filename, "fasta"):
