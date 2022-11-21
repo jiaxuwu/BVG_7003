c978fbe262993a7b6de9b27e4d0ba857d4164099
Jiaxu Wu <99823441+jiaxuwu@users.noreply.github.com>
2022-11-21 01:26:55 -0500

    Delete convert_jxw.py

--- SeqIO/convert_jxw.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Converting a sequence file from .gbk to .fasta file
-from Bio import SeqIO
-input_filename = "NC_000913.gbk"  # Write the .gbk file name here
-output_filename = "NC_000913_converted.fasta"
-count = SeqIO.convert(input_filename, "gb", output_filename, "fasta")
-print(str(count) + " records converted")
-
